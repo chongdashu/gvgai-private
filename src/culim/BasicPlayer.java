0353bf1612986265c58f81c34529c9125e54f335
Chong-U Lim <culim@mit.edu>
2014-03-26 12:04:44 -0400

    Basic player extending AbstractPlayer.

---
+++ src/culim/BasicPlayer.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package culim;
+
+import ontology.Types.ACTIONS;
+import tools.ElapsedCpuTimer;
+import core.game.StateObservation;
+import core.player.AbstractPlayer;
+
+/**
+ * @author culim
+ *
+ */
+public class BasicPlayer extends AbstractPlayer {
+
+	/**
+	 * 
+	 */
+	public BasicPlayer(StateObservation stateObservation, ElapsedCpuTimer timer) {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see core.player.AbstractPlayer#act(core.game.StateObservation, tools.ElapsedCpuTimer)
+	 */
+	@Override
+	public ACTIONS act(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
